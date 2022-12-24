616df2e3e536042c74b64df4382f4ae9ce5ace1b
Mohamad Badawa <7badawa@gmail.com>
2022-12-24 14:50:59 -0500

    Updated getAllRepo

--- routes/api/getAllReports.js
+++ routes/api/getAllReports.js
@@ -1,23 +1,22 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import createJHA from '../../models/CreateJHA.js';
+import createJHA from "../../models/CreateJHA.js";
 
-router.get('/', (req, res) => {
-  try {
-    createJHA
-      .findAll()
-      .then((allReports) => {
-        console.log(allReports);
-        res.json({ allReports });
-        res.status(200);
-      })
-      .catch((err) => {
-        console.log(err.message);
-        res.status(400);
-      });
-  } catch (err) {
-    console.error(err.message);
-  }
+router.get("/", (req, res) => {
+	try {
+		createJHA
+			.findAll()
+			.then((reports) => {
+				res.send(reports);
+				res.status(200);
+			})
+			.catch((err) => {
+				console.log(err.message);
+				res.status(400);
+			});
+	} catch (err) {
+		console.error(err.message);
+	}
 });
 
 export default router;
