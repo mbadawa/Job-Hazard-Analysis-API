d8e6ec93b4267c314fdd9eb521f0451fb7c18ac8
Mohamad Badawa <7badawa@gmail.com>
2022-12-26 18:26:45 -0500

    Added validation and feedback

--- routes/api/updateReport.js
+++ routes/api/updateReport.js
@@ -1,56 +1,76 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import createJHA from "../../models/CreateJHA.js";
+import createJHA from '../../models/CreateJHA.js';
+import { check, validationResult } from 'express-validator';
 
-router.put("/:id", async (req, res) => {
-	const {
-		fullName,
-		superVisorName,
-		companyName,
-		projectName,
-		projectDate,
-		operation,
-		departmentOrUnit,
-		location,
-		indoorOrOutdoor,
-		trainingRequired,
-		ppeRequired,
-		equipmentUsed,
-		chemicalsUsed,
-		additionalComments,
-		employees,
-		analysis,
-	} = req.body;
+router.put(
+  '/:id',
+  [
+    check('fullName', 'Full name is required').not().isEmpty(),
+    check('superVisorName', 'Supervisor name is required').not().isEmpty(),
+    check('companyName', 'Company name is required').not().isEmpty(),
+    check('projectName', 'Project name is required').not().isEmpty(),
+    check('projectDate', 'Project date is required').not().isEmpty(),
+    check('operation', 'Operation title is required').not().isEmpty(),
+    check('departmentOrUnit', 'Department/Unit title is required')
+      .not()
+      .isEmpty(),
+    check('location', 'Location is required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      fullName,
+      superVisorName,
+      companyName,
+      projectName,
+      projectDate,
+      operation,
+      departmentOrUnit,
+      location,
+      indoorOrOutdoor,
+      trainingRequired,
+      ppeRequired,
+      equipmentUsed,
+      chemicalsUsed,
+      additionalComments,
+      employees,
+      analysis,
+    } = req.body;
 
-	try {
-		await createJHA.update(
-			{
-				fullName,
-				superVisorName,
-				companyName,
-				projectName,
-				projectDate,
-				operation,
-				departmentOrUnit,
-				location,
-				indoorOrOutdoor,
-				trainingRequired,
-				ppeRequired,
-				equipmentUsed,
-				chemicalsUsed,
-				additionalComments,
-				employees,
-				analysis,
-			},
-			{
-				where: { id: req.params.id },
-			}
-		);
-		res.send(200);
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send("Server Error");
-	}
-});
+    try {
+      createJHA.update(
+        {
+          fullName,
+          superVisorName,
+          companyName,
+          projectName,
+          projectDate,
+          operation,
+          departmentOrUnit,
+          location,
+          indoorOrOutdoor,
+          trainingRequired,
+          ppeRequired,
+          equipmentUsed,
+          chemicalsUsed,
+          additionalComments,
+          employees,
+          analysis,
+        },
+        {
+          where: { id: req.params.id },
+        }
+      );
+      res.send(200);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 export default router;
